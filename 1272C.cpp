21a2266c4e49bc27c1dfe9a90ec5de39dd4bb878
G2440 <gauravnegi2440@gmail.com>
2020-05-20 23:38:28 +0530

    FORMULA

--- 1272C.cpp
+++ 1272C.cpp
@@ -1,11 +1,12 @@
 // Yet Another Broken Keyboard
 #include<iostream>
 #include<string>
+#define ulli unsigned long long int 
 using namespace std;
 
 int main(){
 
-   int x,y;
+   ulli x,y;
    cin>>x>>y;
    string j;
    cin>>j;
@@ -15,9 +16,9 @@ int main(){
    cin>>c;
    alpha[c-'a']++;
    }
-   int len=0;
-   int sub=0;
-   for(int i =0 ; j[i] !=0;i++){
+   ulli len=0;
+   ulli sub=0;
+   for(ulli i =0 ; j[i] !=0;i++){
        if(alpha[j[i] - 'a'])
        len++;
        else{
